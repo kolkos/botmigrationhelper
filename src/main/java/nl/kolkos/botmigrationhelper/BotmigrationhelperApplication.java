740f364890d6f63df40efe8eb5d4aeca43993985
Kolkos <antonkolk@gmail.com>
2019-07-15 08:56:10 +0200

    spring

---
+++ src/main/java/nl/kolkos/botmigrationhelper/BotmigrationhelperApplication.java
@@ -0,0 +1,13 @@
+package nl.kolkos.botmigrationhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotmigrationhelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotmigrationhelperApplication.class, args);
+	}
+
+}
